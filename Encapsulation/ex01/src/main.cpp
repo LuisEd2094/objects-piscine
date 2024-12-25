b8f3529c312778a7c91dde2303b1132f3ece0bc9
Luis <luissoto2094@gmail.com>
2024-12-25 22:09:00 +0100

    Creates graph with lines

--- Encapsulation/ex01/src/main.cpp
+++ Encapsulation/ex01/src/main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char **argv)
     try{
         if (argc == 1)
         {
-            /* Graph graph;
+            Graph graph;
             graph.addVector(0, 0);
             graph.addVector(2, 2);
             graph.addVector(4, 2);
 
 
             graph.printGraph();
-            std::cout << "Size: " << graph.getSize() << std::endl; */
+            std::cout << "Size: " << graph.getSize() << std::endl;
 
             Graph graph2;
             graph2.addLine(Vector2(0, 0), Vector2(2, 9));
