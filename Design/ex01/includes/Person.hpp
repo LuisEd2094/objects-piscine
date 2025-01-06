918f4ba2297dec8dbed8a24bc23f189bf6d28af9
Luis <luissoto2094@gmail.com>
2025-01-06 13:48:17 +0100

    Created ex01 folder, copied .hpp files, created basic main and test file

---
+++ Design/ex01/includes/Person.hpp
@@ -0,0 +1,35 @@
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
+#include <iostream>
+#include <vector>
+#include "enums.hpp"
+
+class Room;
+class Form;
+class Course;
+class Classroom;
+
+class Person
+{
+private:
+    std::string _name;
+    Room *_currentRoom;
+
+public:
+    Person(std::string p_name);
+    Room *room() { return (_currentRoom); }
+};
+
+
+class Student : public Person
+{
+private:
+    std::vector<Course *> _subscribedCourse;
+
+public:
+    void attendClass(Classroom *p_classroom);
+    void exitClass();
+    void graduate(Course *p_course);
+};
+#endif
---
+++ Design/ex01/includes/enums.hpp
@@ -0,0 +1,19 @@
+#ifndef ENUMS_HPP
+# define ENUMS_HPP
+
+
+enum class FormType
+{
+	CourseFinished,
+	NeedMoreClassRoom,
+	NeedCourseCreation,
+    SubscriptionToCourse
+};
+
+enum class Event
+{
+	RingBell
+};
+
+
+#endif  
