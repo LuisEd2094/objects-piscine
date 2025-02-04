8f381cd38dc0aae797746a5857509070bde1e1f9
Luis <luissoto2094@gmail.com>
2025-02-04 22:01:06 +0100

    refactor: moved code blocks to their own functions

--- Encapsulation/ex01/src/PNGWriter.cpp
+++ Encapsulation/ex01/src/PNGWriter.cpp
@@ -70,23 +70,27 @@ void PNGWriter::writeIHDR(std::ofstream &file, uint32_t width, uint32_t height)
     writeChunk(file, ihdrChunk);
 }
 
-void PNGWriter::writeIDAT(std::ofstream &file, const std::vector<std::vector<std::vector<uint8_t> > > &image)
+std::vector<unsigned char>  flatten_data(const std::vector<std::vector<std::vector<uint8_t> > > &image)
 {
-    std::vector<unsigned char> uncompressed_data;
-
+    std::vector<uint8_t> flattened_data;
     for (size_t y = 0; y < image.size(); y++)
     {
-        uncompressed_data.push_back(0); 
+        flattened_data.push_back(0);
         for (size_t x = 0; x < image[y].size(); x++)
         {
-            uncompressed_data.push_back(image[y][x][0]); // Red
-            uncompressed_data.push_back(image[y][x][1]); // Green
-            uncompressed_data.push_back(image[y][x][2]); // Blue
+            flattened_data.push_back(image[y][x][0]); // Red
+            flattened_data.push_back(image[y][x][1]); // Green
+            flattened_data.push_back(image[y][x][2]); // Blue
         }
     }
+    return flattened_data;
+}
+
+std::vector <unsigned char> get_idat(std::vector<unsigned char>& uncompressed_data)
+{
+    // zlib header (0x78, 0x01) indicates no compression
     std::vector<unsigned char> idat_data;
 
-    // zlib header (0x78, 0x01) indicates no compression
     idat_data.push_back(0x78);
     idat_data.push_back(0x01);
 
@@ -94,16 +98,23 @@ void PNGWriter::writeIDAT(std::ofstream &file, const std::vector<std::vector<std
     size_t data_size = uncompressed_data.size();
     idat_data.push_back(0x01);  // Final block (BFINAL=1), no compression (BTYPE=00)
     
-    // Little-endian length and one's complement
+    // Little-endian length and one's complement, this is for uncompressed data
     idat_data.push_back(data_size & 0xFF);           // LEN LSB
     idat_data.push_back((data_size >> 8) & 0xFF);    // LEN MSB
     idat_data.push_back(~data_size & 0xFF);          // NLEN LSB (one's complement)
     idat_data.push_back((~data_size >> 8) & 0xFF);   // NLEN MSB
 
-    // Step 3: Append the raw uncompressed image data
     idat_data.insert(idat_data.end(), uncompressed_data.begin(), uncompressed_data.end());
 
+    return idat_data;
+}
+
+void PNGWriter::writeIDAT(std::ofstream &file, const std::vector<std::vector<std::vector<uint8_t> > > &image)
+{
+    std::vector<unsigned char> uncompressed_data = flatten_data(image);
+    std::vector<unsigned char> idat_data = get_idat(uncompressed_data);
     std::vector<unsigned char> idat_chunk;
+    
     idat_chunk.push_back('I');
     idat_chunk.push_back('D');
     idat_chunk.push_back('A');
